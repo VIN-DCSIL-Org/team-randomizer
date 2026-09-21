28242697d914dd47d83de67505b4ee0d1f6b7169
shahbazsingh <shahbaz.nanda@mail.utoronto.ca>
2026-09-21 17:59:53 -0400

    homepage renaming

---
+++ my-react-app/src/Home.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function Home() {
+  return (
+    <main className="homepage">
+      <h1>Home Page</h1>
+      <p>Build the team randomizer from here.</p>
+    </main>
+  )
+}
+
+export default Home
